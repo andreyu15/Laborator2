54bb1ac64ca40c4f69715dcae1534720ebf6f8f1
andreyu15 <97602014+andreyu15@users.noreply.github.com>
2022-01-12 11:51:43 +0000

    Update orientare.js

--- orientare.js
+++ orientare.js
@@ -1,7 +1,8 @@
 window.addEventListener("deviceorientation", on_device_orientation);
 function on_device_orientation(evt)
 {
-var alpha = evt.alpha;var beta = evt.beta;
+var alpha = evt.alpha;
+ var beta = evt.beta;
 var gamma = evt.gamma;
 document.getElementById("a").innerHTML = "alpha = "+alpha;
 document.getElementById("b").innerHTML = "beta = "+beta;
@@ -16,4 +17,5 @@ context.translate(centru.x/2, centru.y/2);
 context.rotate(alpha * Math.PI / 180);
 context.beginPath();
   context.rect(-latura_patrat/2,-latura_patrat/2, latura_patrat, latura_patrat);
-context.stroke();}
+context.stroke();
+}

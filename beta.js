40e15e65fe29f5f156367c9c4aeae259d34be48f
andreyu15 <97602014+andreyu15@users.noreply.github.com>
2022-01-14 18:15:30 +0000

    Update beta.js

--- beta.js
+++ beta.js
@@ -8,7 +8,8 @@ document.getElementById("b").innerHTML = "beta = "+beta;
 document.getElementById("c").innerHTML = "gamma = "+gamma;
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
-var centru = {x:canvas.width/2, y:canvas.height/2}var latura_patrat = 50;
+var centru = {x:canvas.width/2, y:canvas.height/2}
+var latura_patrat = 50;
 context.clearRect(0, 0, canvas.width, canvas.height);
 context.fillStyle = "#6A0888";
 context.beginPath();
